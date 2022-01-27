c56cebd98ae6a8439b7634bdf2dd74c4d5e7736c
akhmed0606 <analgiev57gmail.com>
2022-01-26 21:46:38 -0500

    Fetched data using axios and console loged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [articles, setArticles] = useState([])
+  const [query, setQuery] = useState('everything')
+  const [load, setLoad] = useState(true)
+
+  const fetchNews = async () => {
+    await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.REACT_APP_API_KEY}`).then((res) => {
+      const articles =  res.data
+      console.log(articles)
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+    }
+
+  useEffect(() => {
+     fetchNews()
+  },[])
+  
   return (
     <div className="App">
       Hello World

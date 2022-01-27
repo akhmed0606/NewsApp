773f1c8eaee81c240babeb2c9a663a952db69187
akhmed0606 <analgiev57gmail.com>
2022-01-27 12:45:29 -0500

    Fetched data  and destructed objects in RC

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const fetchNews = async () => {
     await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.REACT_APP_API_KEY}`).then((res) => {
       const articles =  res.data
-      console.log(articles)
+      setArticles(articles.response.docs)
     })
     .catch((error) => {
       console.log(error)
@@ -21,9 +21,28 @@ function App() {
   },[])
   
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <section>
+      {articles.map((article) => {
+        const {abstract, headline:{main}, news_desk, web_url, word_count, byline:{original},section_name, _id, lead_paragraph} = article
+
+        return(
+          <article key={_id}>
+            <h2>{main}</h2>
+            <h4>{abstract}</h4>
+            <a href={web_url} target='_blank'>
+              Web Resource
+            </a>
+            <p>{lead_paragraph}</p>
+            <ul>
+              <li>{original}</li>
+              <li>{news_desk}</li>
+              <li>{section_name}</li>
+              <li>{word_count}</li>
+            </ul>
+          </article>
+        )
+      })}
+    </section>
   );
 }
 

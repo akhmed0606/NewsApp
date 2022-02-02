d198086c4a5a3c9c674793860667094574fabbcd
akhmed0606 <analgiev57gmail.com>
2022-02-02 15:37:41 -0500

    start testing in react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, cleanup, waitFor } from '@testing-library/react'
+import "@testing-library/jest-dom";
+import axiosMock from 'axios'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup)
+
+it("fetches and displays data", async() => {
+
+axiosMock.get.mockResolvedValue({ articles: {} })
+ 
+let query = 'everything'
+
+const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.REACT_APP_API_KEY}`
+
+const { getByTestId } = render(<App url={url} />)
+expect(getByTestId("checkTitle")).toHaveTextContent('Articles about')
+
+
+ const resolvedData =  await waitFor(() => getByTestId("article"));
+ expect(resolvedData).toHaveTextContent("");
+ expect(axiosMock.get).toHaveBeenCalledTimes(1);
+ expect(axiosMock.get).toHaveBeenCalledWith(url);
+
+})
+
